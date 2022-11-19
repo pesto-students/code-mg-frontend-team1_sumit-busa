7e52785469852b94cf7c2e3c9ce2d8a0b5b4dec1
SurajWadje <suraj@litmusblox.io>
2022-11-19 12:17:49 +0530

    add : api integration for assignment page

--- src/components/student/MonacoEditor.tsx
+++ src/components/student/MonacoEditor.tsx
@@ -1,14 +1,77 @@
-import React from 'react'
+import { useCallback, useState } from "react";
+import _ from "lodash";
 import Editor from "@monaco-editor/react";
+import useSocket from "../../hooks/useSocket";
+import { Button } from "@mui/material";
 
 function MonacoEditor() {
+  const [code, setCode] = useState<string | undefined>("");
+  const [isDirty, setDirty] = useState(false);
+  const [output, setOutput] = useState("");
+  const [customInput, setCustomInput] = useState("");
+  const { isConnected, registerEvent } = useSocket();
+
+  const saveCode = registerEvent("save", (payload) => {
+    console.log(payload);
+    if (payload === "success") setDirty(false);
+    else alert("Error saving code");
+  });
+
+  const customRun = registerEvent("customRun", (payload) => {
+    console.log({ payload });
+    if (payload?.status?.id === 6) setOutput(payload.compile_output);
+    else setOutput(payload.stdout);
+  });
+
+  const handleChange = (e: string | undefined) => {
+    setCode(e);
+    setDirty(true);
+    if (e === undefined) return;
+    debounceHandleSave(e);
+  };
+
+  const handleSaveCode = (code: string) => {
+    saveCode({ assignmentId: 1, language: "C", sourceCode: code });
+  };
+
+  const handleRunCode = () => {
+    console.log(code);
+    customRun({ sourceCode: code, language: "C", stdin: customInput });
+  };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debounceHandleSave = useCallback(
+    _.debounce(handleSaveCode, 1000, { maxWait: 30000 }),
+    []
+  );
   return (
-    <Editor
-     height="90vh"
-     defaultLanguage="javascript"
-     defaultValue="// some comment"
-   />
-  )
+    <div>
+      <div>
+        {" "}
+        {isConnected ? "connected" : "Not connected"}{" "}
+        <div>{isDirty ? "Saving..." : "saved"}</div>
+      </div>
+      <Editor
+        height="60vh"
+        defaultLanguage="C"
+        defaultValue={code}
+        theme="vs-dark"
+        onChange={handleChange}
+      />
+      <Button variant="outlined" onClick={handleRunCode}>
+        Run Code
+      </Button>
+      <input
+        type="text"
+        value={customInput}
+        onChange={(e) => setCustomInput(e.target.value)}
+      ></input>
+      <div>
+        {output?.split("\n")?.map((line) => (
+          <p key={line}>{line}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default MonacoEditor
+export default MonacoEditor;
--- src/components/student/StudentAssignmentScreen.tsx
+++ src/components/student/StudentAssignmentScreen.tsx
@@ -1,25 +1,20 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import MonacoEditor from "./MonacoEditor";
+import { useAssignmentQuery } from "../../services/api";
 
 function StudentAssignmentScreen() {
+  const { isLoading, data } = useAssignmentQuery(1);
+  if (isLoading) return <>Loading....</>;
   return (
     <Grid container spacing={2}>
       <Grid item xs={6}>
-        Question
-        <br /> Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        <h1>{data?.title}</h1>
+        {data?.problemStatement}
       </Grid>
       <Grid item xs={6}>
         <div style={{}}>
-        <MonacoEditor />
+          <MonacoEditor />
         </div>
       </Grid>
     </Grid>
