4a3d6f1a92eddc2cd81f0e150cf5073b21e68fd6
SurajWadje <suraj@litmusblox.io>
2022-11-20 19:24:40 +0530

    add : react-router integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import "./App.css";
 import StudentAssignmentScreen from "./components/student/StudentAssignmentScreen";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <StudentAssignmentScreen />
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>}></Route>
+          <Route
+            path="/student/assignment"
+            element={<StudentAssignmentScreen />}
+          ></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
