9c4333b12cc3c7517feb1de3966eb4cc9f8034f9
SurajWadje <suraj@litmusblox.io>
2022-12-03 17:55:27 +0530

    fix : useSocket multiple connections

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,9 +1,11 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
 console.log(process.env.REACT_APP_BASE_URL);
 const baseUrl: string = process.env.REACT_APP_BASE_URL || "";
 const token = localStorage.token;
+const socket = io(baseUrl, {
+  extraHeaders: { Authorization: `Bearer ${token}` },
+});
 
 export const EVENTS = {
   submit: "assignment:submit",
@@ -12,30 +14,14 @@ export const EVENTS = {
 } as const;
 
 type EventKeys = keyof typeof EVENTS;
-
 const useSocket = () => {
   const [isConnected, setIsConnected] = useState(false);
-  const socket = io(baseUrl, {
-    extraHeaders: { Authorization: `Bearer ${token}` },
-  });
-
-  const eventHandlers: Record<string, { handler: (payload: any) => void }> =
-    useMemo(() => {
-      return {};
-    }, []);
-
-  const registerEvent = useMemo(
-    () => (eventName: EventKeys, handler: (payload: any) => void) => {
-      eventHandlers[eventName] = { handler };
-      return (payload: any) => {
-        console.log("triggered", eventName);
-        socket.emit(EVENTS[eventName], payload);
-      };
-    },
-    []
-  );
 
   useEffect(() => {
+    if (socket.active) {
+      setIsConnected(true);
+    }
+
     socket.on("connect", () => {
       console.log("connect");
       setIsConnected(true);
@@ -52,6 +38,22 @@ const useSocket = () => {
     };
   }, []);
 
+  const eventHandlers: Record<string, { handler: (payload: any) => void }> =
+    useMemo(() => {
+      return {};
+    }, []);
+
+  const registerEvent = useMemo(
+    () => (eventName: EventKeys, handler: (payload: any) => void) => {
+      eventHandlers[eventName] = { handler };
+      return (payload: any) => {
+        console.log("triggered", eventName);
+        socket.emit(EVENTS[eventName], payload);
+      };
+    },
+    [eventHandlers]
+  );
+
   useEffect(() => {
     Object.keys(EVENTS).forEach((event) => {
       socket.on(EVENTS[event as EventKeys], (payload) => {
@@ -61,14 +63,14 @@ const useSocket = () => {
           typeof eventHandlers[eventName].handler === "function"
         )
           console.log({ event, payload });
-          eventHandlers[eventName].handler(payload);
+        eventHandlers[eventName].handler(payload);
       });
     });
 
     return () => {
       Object.keys(EVENTS).forEach((event) => socket.off(event));
     };
-  }, []);
+  }, [eventHandlers]);
 
   return { isConnected, registerEvent };
 };
