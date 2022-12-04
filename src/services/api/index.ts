ca80fd15f94fd2fac106ad3ad133e605c9f21a79
SurajWadje <suraj@litmusblox.io>
2022-12-04 14:11:23 +0530

    fix : token

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,19 +1,21 @@
 import { LoginPayload, LoginResponse } from "../../models/api.interface";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { AddStudentRequest, Assignment, ClassRequest, ClassResponse } from "./api.interface";
+import {
+  AddStudentRequest,
+  Assignment,
+  ClassRequest,
+  ClassResponse,
+  CreateAssignment,
+} from "./api.interface";
 
 export const api = createApi({
   reducerPath: "codeMgAPI",
   baseQuery: fetchBaseQuery({
-    // baseUrl: `${process.env.REACT_APP_BASE_URL}/api`,
-    baseUrl: "http://localhost:8080/api",
+    baseUrl: `${process.env.REACT_APP_BASE_URL}/api`,
     prepareHeaders: (headers) => {
-      // const token = localStorage.token || "";
-      const token =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ0ZWFjaGVyQHRlc3QuY29tIiwiZnVsbE5hbWUiOiJ0ZWFjaGVyIHVzZXIiLCJyb2xlIjoiVGVhY2hlciIsImlhdCI6MTY3MDA1Njk2NSwiZXhwIjoxNjcwMTQzMzY1fQ.WKIbbgLa7wtTCen3smaUjrsG0KRHTmugnH80nE4gq5Q";
+      const token = localStorage.token || "";
       headers.set("Access-Control-Allow-Origin", "*");
       headers.set("Authorization", "Bearer " + token);
-      // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ0ZWFjaGVyQHRlc3QuY29tIiwiZnVsbE5hbWUiOiJ0ZWFjaGVyIHVzZXIiLCJyb2xlIjoiVGVhY2hlciIsImlhdCI6MTY3MDA1Njk2NSwiZXhwIjoxNjcwMTQzMzY1fQ.WKIbbgLa7wtTCen3smaUjrsG0KRHTmugnH80nE4gq5Q
       return headers;
     },
   }),
@@ -35,14 +37,14 @@ export const api = createApi({
         url: `student/assignment/${id}`,
       }),
     }),
-
-    createAssignment: builder.mutation({
+    createAssignment: builder.mutation<void, CreateAssignment>({
       query: (assignment) => ({
         url: "/assignment",
         method: "POST",
         body: assignment,
       }),
     }),
+
     createClass: builder.mutation<any, ClassRequest>({
       query: (newclass) => ({
         url: "/class",
@@ -51,13 +53,13 @@ export const api = createApi({
       }),
       invalidatesTags: ["class"],
     }),
-    addStudent: builder.mutation<any,AddStudentRequest>({
+    addStudent: builder.mutation<any, AddStudentRequest>({
       query: (student) => ({
         url: "class/add-students",
         method: "POST",
         body: student,
       }),
-      invalidatesTags:['class']
+      invalidatesTags: ["class"],
     }),
     getClass: builder.query<ClassResponse[], void>({
       query: () => ({ url: "/class" }),
