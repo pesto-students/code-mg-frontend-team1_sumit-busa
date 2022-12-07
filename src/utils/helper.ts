3ab7929b29ff7af2442775f9bea321a67818f8b3
SurajWadje <suraj14wadje@gmail.com>
2022-12-07 20:49:19 +0530

    add : due date helper function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,4 +3,12 @@ import _ from 'lodash';
 export const isNilOrEmpty = (value:any) =>
   _.isNil(value) || _.isEmpty(value) || _.isNull(value) || _.isNaN(value);
 
+export const getFormattedDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+};
+
 export const isPresent = (value:any) => !isNilOrEmpty(value);
